4551bfe09e3a7e8135ccfb6837f1d3df216494fa
vanshika <vanshika@example.com>
2024-02-28 00:09:30 -0800

    changes made

--- firstjava.java
+++ firstjava.java
@@ -1,6 +1,6 @@
    public class FirstJava
     {
-      public sttaic void main(String[] args)
+      public static void main(String[] args)
        {
          System.out.pritnln("Hello! Welcome to Jenkins!") ;
        }
